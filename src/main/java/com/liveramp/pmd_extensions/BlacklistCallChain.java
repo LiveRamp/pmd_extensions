9213d8e89184f1b4c66767d9009e92a5bc522aa5
Ben Podgursky <bpodgursky@liveramp.com>
2014-08-12 21:36:09 -0700

    add a class for banning a call chain.  very very alpha, but it works in this case

---
+++ src/main/java/com/liveramp/pmd_extensions/BlacklistCallChain.java
@@ -0,0 +1,93 @@
+package com.liveramp.pmd_extensions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTName;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.rule.properties.StringProperty;
+
+//  TODO this is horribly non-generic, but a first shot at catching call chains
+public class BlacklistCallChain extends AbstractJavaRule {
+
+  private static final String CALL_CHAIN = "BlacklistCallChain.CallChain";
+  private static final String CLASS_LIST = "BlacklistCallChain.ClassesToInspect";
+
+  public BlacklistCallChain() {
+    definePropertyDescriptor(new StringProperty(CALL_CHAIN, "List of calls to blacklist", "", 0));
+    definePropertyDescriptor(new StringProperty(CLASS_LIST, "List of classes to check", "", 0));
+  }
+
+  @Override
+  public void start(RuleContext ctx) {
+    Set<List<String>> chains = Sets.newHashSet();
+    Object prop = getProperty(getPropertyDescriptor(CALL_CHAIN));
+    for (String reference : prop.toString().split(",")) {
+      chains.add(Arrays.asList(reference.trim().split("\\.")));
+    }
+    ctx.setAttribute(CALL_CHAIN, chains);
+    BlacklistMethodHelper.setContext(null, CLASS_LIST, ctx, this);
+
+    super.start(ctx);
+  }
+
+  public static Set<List<String>> getCallsFromContext(Object data){
+    RuleContext ctx = (RuleContext)data;
+    return (Set<List<String>>)ctx.getAttribute(CALL_CHAIN);
+  }
+
+  @Override
+  public Object visit(ASTPrimaryExpression node, Object data) {
+    Set<List<String>> badCalls = getCallsFromContext(data);
+    List<String> affectedClasses = BlacklistMethodHelper.getClassesFromContext(data, CLASS_LIST);
+
+    List<String> parts = Lists.newArrayList();
+    for (int i = 0; i < node.jjtGetNumChildren(); i++) {
+      List<String> image = image(node.jjtGetChild(i));
+      if(!image.isEmpty()){
+        parts.addAll(image);
+      }
+    }
+
+    if(badCalls.contains(parts)){
+      if(affectedClasses.isEmpty()){
+        addViolation(data, node);
+      }else{
+        PmdHelper.checkParentClasses(node, data, affectedClasses, node.jjtGetParent(), this);
+      }
+    }
+
+    return super.visit(node, data);
+  }
+
+  private List<String> image(Node node){
+    if(node instanceof ASTPrimaryPrefix){
+      ASTPrimaryPrefix prefix = (ASTPrimaryPrefix) node;
+
+      List<String> results = Lists.newArrayList();
+      for (int i = 0; i < prefix.jjtGetNumChildren(); i++) {
+        results.addAll(image(prefix.jjtGetChild(i)));
+      }
+      return results;
+
+    }else if(node instanceof ASTPrimarySuffix){
+      if(node.getImage() != null) {
+        return Lists.newArrayList(node.getImage().split("\\."));
+      }
+    }else if(node instanceof ASTName){
+      if(node.getImage() != null) {
+        return Lists.newArrayList(node.getImage().split("\\."));
+      }
+    }
+    return Lists.newArrayList();
+  }
+
+}
